ce1ea684a2005358fd96780177f398a8c0b95b43
deepakj98 <deepakj98@outlook.in>
2026-04-06 10:51:22 +0530

    removed service rspec

--- spec/services/crypto_price_fetcher_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe CryptoPriceFetcher do
-  it "returns nil on exception" do
-    allow(described_class).to receive(:get).and_raise(StandardError)
-
-    expect(described_class.fetch("bitcoin")).to be_nil
-  end
-end
